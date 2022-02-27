eb3873d6e742f7a8e359630d8ff513b1786232ed
masayoshi64 <sho32001025@gmail.com>
2022-02-27 15:15:01 +0900

    add abc239

---
+++ abc239/e/main.cpp
@@ -0,0 +1,480 @@
+/* #region header */
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+// types
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+typedef pair<ll, ll> Pl;
+typedef pair<int, int> Pi;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<char> vc;
+template <typename T> using mat = vector<vector<T>>;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvl;
+typedef vector<vector<char>> vvc;
+// abreviations
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rep_(i, a_, b_, a, b, ...) for (ll i = (a), max_i = (b); i < max_i; i++)
+#define rep(i, ...) rep_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define rrep_(i, a_, b_, a, b, ...) for (ll i = (b - 1), min_i = (a); i >= min_i; i--)
+#define rrep(i, ...) rrep_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+#define srep(i, a, b, c) for (ll i = (a), max_i = (b); i < max_i; i += c)
+#define SZ(x) ((int)(x).size())
+#define pb(x) push_back(x)
+#define eb(x) emplace_back(x)
+#define mp make_pair
+//入出力
+#define print(x) cout << x << endl
+template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
+    for (auto &e : v)
+        cout << e << " ";
+    cout << endl;
+    return os;
+}
+void scan(int &a) {
+    cin >> a;
+}
+void scan(long long &a) {
+    cin >> a;
+}
+void scan(char &a) {
+    cin >> a;
+}
+void scan(double &a) {
+    cin >> a;
+}
+void scan(string &a) {
+    cin >> a;
+}
+template <class T> void scan(vector<T> &a) {
+    for (auto &i : a)
+        scan(i);
+}
+#define vsum(x) accumulate(all(x), 0LL)
+#define vmax(a) *max_element(all(a))
+#define vmin(a) *min_element(all(a))
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+// functions
+// gcd(0, x) fails.
+ll gcd(ll a, ll b) {
+    return b ? gcd(b, a % b) : a;
+}
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+ll safemod(ll a, ll b) {
+    return (a % b + b) % b;
+}
+template <class T> bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T> bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <typename T> T mypow(T x, ll n) {
+    T ret = 1;
+    while (n > 0) {
+        if (n & 1)
+            (ret *= x);
+        (x *= x);
+        n >>= 1;
+    }
+    return ret;
+}
+ll modpow(ll x, ll n, const ll mod) {
+    ll ret = 1;
+    while (n > 0) {
+        if (n & 1)
+            (ret *= x);
+        (x *= x);
+        n >>= 1;
+        x %= mod;
+        ret %= mod;
+    }
+    return ret;
+}
+
+uint64_t my_rand(void) {
+    static uint64_t x = 88172645463325252ULL;
+    x = x ^ (x << 13);
+    x = x ^ (x >> 7);
+    return x = x ^ (x << 17);
+}
+int popcnt(ull x) {
+    return __builtin_popcountll(x);
+}
+template <typename T> vector<int> IOTA(vector<T> a) {
+    int n = a.size();
+    vector<int> id(n);
+    iota(all(id), 0);
+    sort(all(id), [&](int i, int j) { return a[i] < a[j]; });
+    return id;
+}
+struct Timer {
+    clock_t start_time;
+    void start() {
+        start_time = clock();
+    }
+    int lap() {
+        // return x ms.
+        return (clock() - start_time) * 1000 / CLOCKS_PER_SEC;
+    }
+};
+template <int Mod> struct modint {
+    int x;
+
+    modint() : x(0) {
+    }
+
+    modint(long long y) : x(y >= 0 ? y % Mod : (Mod - (-y) % Mod) % Mod) {
+    }
+
+    modint &operator+=(const modint &p) {
+        if ((x += p.x) >= Mod)
+            x -= Mod;
+        return *this;
+    }
+
+    modint &operator-=(const modint &p) {
+        if ((x += Mod - p.x) >= Mod)
+            x -= Mod;
+        return *this;
+    }
+
+    modint &operator*=(const modint &p) {
+        x = (int)(1LL * x * p.x % Mod);
+        return *this;
+    }
+
+    modint &operator/=(const modint &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+
+    modint operator-() const {
+        return modint(-x);
+    }
+
+    modint operator+(const modint &p) const {
+        return modint(*this) += p;
+    }
+
+    modint operator-(const modint &p) const {
+        return modint(*this) -= p;
+    }
+
+    modint operator*(const modint &p) const {
+        return modint(*this) *= p;
+    }
+
+    modint operator/(const modint &p) const {
+        return modint(*this) /= p;
+    }
+
+    bool operator==(const modint &p) const {
+        return x == p.x;
+    }
+
+    bool operator!=(const modint &p) const {
+        return x != p.x;
+    }
+
+    modint inverse() const {
+        int a = x, b = Mod, u = 1, v = 0, t;
+        while (b > 0) {
+            t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return modint(u);
+    }
+
+    modint pow(int64_t n) const {
+        modint ret(1), mul(x);
+        while (n > 0) {
+            if (n & 1)
+                ret *= mul;
+            mul *= mul;
+            n >>= 1;
+        }
+        return ret;
+    }
+
+    friend ostream &operator<<(ostream &os, const modint &p) {
+        return os << p.x;
+    }
+
+    friend istream &operator>>(istream &is, modint &a) {
+        long long t;
+        is >> t;
+        a = modint<Mod>(t);
+        return (is);
+    }
+
+    static int get_mod() {
+        return Mod;
+    }
+
+    constexpr int get() const {
+        return x;
+    }
+};
+template <typename T = int> struct Edge {
+    int from, to;
+    T cost;
+    int idx;
+
+    Edge() = default;
+
+    Edge(int from, int to, T cost = 1, int idx = -1) : from(from), to(to), cost(cost), idx(idx) {
+    }
+
+    operator int() const {
+        return to;
+    }
+};
+
+template <typename T = int> struct Graph {
+    vector<vector<Edge<T>>> g;
+    int es;
+
+    Graph() = default;
+
+    explicit Graph(int n) : g(n), es(0) {
+    }
+
+    size_t size() const {
+        return g.size();
+    }
+
+    void add_directed_edge(int from, int to, T cost = 1) {
+        g[from].emplace_back(from, to, cost, es++);
+    }
+
+    void add_edge(int from, int to, T cost = 1) {
+        g[from].emplace_back(from, to, cost, es);
+        g[to].emplace_back(to, from, cost, es++);
+    }
+
+    void read(int M, int padding = -1, bool weighted = false, bool directed = false) {
+        for (int i = 0; i < M; i++) {
+            int a, b;
+            cin >> a >> b;
+            a += padding;
+            b += padding;
+            T c = T(1);
+            if (weighted)
+                cin >> c;
+            if (directed)
+                add_directed_edge(a, b, c);
+            else
+                add_edge(a, b, c);
+        }
+    }
+};
+
+/* #endregion*/
+// constant
+#define inf 1000000000ll
+#define INF 4000000004000000000LL
+#define mod 998244353ll
+#define endl '\n'
+const long double eps = 0.000000000000001;
+const long double PI = 3.141592653589793;
+using mint = modint<mod>;
+using vmint = vector<mint>;
+struct SuccinctIndexableDictionary {
+    size_t length;
+    size_t blocks;
+    vector<unsigned> bit, sum;
+
+    SuccinctIndexableDictionary() = default;
+
+    SuccinctIndexableDictionary(size_t length) : length(length), blocks((length + 31) >> 5) {
+        bit.assign(blocks, 0U);
+        sum.assign(blocks, 0U);
+    }
+
+    void set(int k) {
+        bit[k >> 5] |= 1U << (k & 31);
+    }
+
+    void build() {
+        sum[0] = 0U;
+        for (int i = 1; i < blocks; i++) {
+            sum[i] = sum[i - 1] + __builtin_popcount(bit[i - 1]);
+        }
+    }
+
+    bool operator[](int k) {
+        return (bool((bit[k >> 5] >> (k & 31)) & 1));
+    }
+
+    int rank(int k) {
+        return (sum[k >> 5] + __builtin_popcount(bit[k >> 5] & ((1U << (k & 31)) - 1)));
+    }
+
+    int rank(bool val, int k) {
+        return (val ? rank(k) : k - rank(k));
+    }
+};
+template <typename T, int MAXLOG> struct WaveletMatrix {
+    size_t length;
+    SuccinctIndexableDictionary matrix[MAXLOG];
+    int mid[MAXLOG];
+
+    WaveletMatrix() = default;
+
+    WaveletMatrix(vector<T> v) : length(v.size()) {
+        vector<T> l(length), r(length);
+        for (int level = MAXLOG - 1; level >= 0; level--) {
+            matrix[level] = SuccinctIndexableDictionary(length + 1);
+            int left = 0, right = 0;
+            for (int i = 0; i < length; i++) {
+                if (((v[i] >> level) & 1)) {
+                    matrix[level].set(i);
+                    r[right++] = v[i];
+                } else {
+                    l[left++] = v[i];
+                }
+            }
+            mid[level] = left;
+            matrix[level].build();
+            v.swap(l);
+            for (int i = 0; i < right; i++) {
+                v[left + i] = r[i];
+            }
+        }
+    }
+
+    pair<int, int> succ(bool f, int l, int r, int level) {
+        return {matrix[level].rank(f, l) + mid[level] * f, matrix[level].rank(f, r) + mid[level] * f};
+    }
+
+    // v[k]
+    T access(int k) {
+        T ret = 0;
+        for (int level = MAXLOG - 1; level >= 0; level--) {
+            bool f = matrix[level][k];
+            if (f)
+                ret |= T(1) << level;
+            k = matrix[level].rank(f, k) + mid[level] * f;
+        }
+        return ret;
+    }
+
+    T operator[](const int &k) {
+        return access(k);
+    }
+
+    // count i s.t. (0 <= i < r) && v[i] == x
+    int rank(const T &x, int r) {
+        int l = 0;
+        for (int level = MAXLOG - 1; level >= 0; level--) {
+            tie(l, r) = succ((x >> level) & 1, l, r, level);
+        }
+        return r - l;
+    }
+
+    // k-th(0-indexed) smallest number in v[l,r)
+    T kth_smallest(int l, int r, int k) {
+        assert(0 <= k && k < r - l);
+        T ret = 0;
+        for (int level = MAXLOG - 1; level >= 0; level--) {
+            int cnt = matrix[level].rank(false, r) - matrix[level].rank(false, l);
+            bool f = cnt <= k;
+            if (f) {
+                ret |= T(1) << level;
+                k -= cnt;
+            }
+            tie(l, r) = succ(f, l, r, level);
+        }
+        return ret;
+    }
+
+    // k-th(0-indexed) largest number in v[l,r)
+    T kth_largest(int l, int r, int k) {
+        return kth_smallest(l, r, r - l - k - 1);
+    }
+
+    // count i s.t. (l <= i < r) && (v[i] < upper)
+    int range_freq(int l, int r, T upper) {
+        int ret = 0;
+        for (int level = MAXLOG - 1; level >= 0; level--) {
+            bool f = ((upper >> level) & 1);
+            if (f)
+                ret += matrix[level].rank(false, r) - matrix[level].rank(false, l);
+            tie(l, r) = succ(f, l, r, level);
+        }
+        return ret;
+    }
+
+    // count i s.t. (l <= i < r) && (lower <= v[i] < upper)
+    int range_freq(int l, int r, T lower, T upper) {
+        return range_freq(l, r, upper) - range_freq(l, r, lower);
+    }
+
+    // max v[i] s.t. (l <= i < r) && (v[i] < upper)
+    T prev_value(int l, int r, T upper) {
+        int cnt = range_freq(l, r, upper);
+        return cnt == 0 ? T(-1) : kth_smallest(l, r, cnt - 1);
+    }
+
+    // min v[i] s.t. (l <= i < r) && (lower <= v[i])
+    T next_value(int l, int r, T lower) {
+        int cnt = range_freq(l, r, lower);
+        return cnt == r - l ? T(-1) : kth_smallest(l, r, cnt);
+    }
+};
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cout << setprecision(30) << fixed;
+    ll n, q;
+    cin >> n >> q;
+    vl x(n);
+    scan(x);
+    Graph g(n);
+    g.read(n - 1, -1, false, false);
+    vl left(n), right(n);
+    vl euler_tour;
+    auto dfs = [&](int v, int p, auto &dfs) -> void {
+        left[v] = euler_tour.size();
+        euler_tour.pb(v);
+        for (auto &e : g.g[v]) {
+            if (e.to == p)
+                continue;
+            dfs(e.to, v, dfs);
+        }
+        right[v] = euler_tour.size();
+        euler_tour.pb(v);
+    };
+    dfs(0, -1, dfs);
+    vl xx(n * 2);
+    rep(i, n) {
+        xx[left[i]] = x[i];
+        xx[right[i]] = x[i];
+    }
+    WaveletMatrix<ll, 32> wm(xx);
+    rep(i, q) {
+        ll v, k;
+        cin >> v >> k;
+        v--;
+        print(wm.kth_largest(left[v], right[v] + 1, k * 2 - 1));
+    }
+}
